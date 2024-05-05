e558e78d1ddc8b4fb969925f9386bbf9343d1a49
DiegoAVega <dvegacampos02@gmail.com>
2024-05-05 07:20:46 -0500

    add presentation to home activity

--- app/src/main/java/com/example/coursemanager/MainActivity.kt
+++ app/src/main/java/com/example/coursemanager/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.coursemanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -11,7 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            println("transicion")
+            val intent =  Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
         }, 2000)
     }
 }
