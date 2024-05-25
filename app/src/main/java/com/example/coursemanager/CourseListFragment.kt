4fc475d669de362f01b121514fc06c2b4013f78d
DiegoAVega <55397337+DiegoAVega@users.noreply.github.com>
2024-05-25 08:09:25 -0500

    union por fragment

--- app/src/main/java/com/example/coursemanager/CourseListFragment.kt
+++ app/src/main/java/com/example/coursemanager/CourseListFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class CourseListFragment : Fragment() {
+    lateinit var addButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,9 +23,11 @@ class CourseListFragment : Fragment() {
         val recyclerView: RecyclerView = view.findViewById(R.id.CoursesRecyclerView1)
         recyclerView.layoutManager = LinearLayoutManager(view.context)
         recyclerView.adapter = CoursesAdapter(courses) { course ->
-
+        }
+        addButton=view.findViewById(R.id.btnCircular1)
+        addButton.setOnClickListener{
+            findNavController().navigate(R.id.action_courseListFragment_to_addTaskFragment)
         }
         return view
     }
-
 }
